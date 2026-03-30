22ec0f28515ba129b3fe50f84bb90080b1ed8312
SoftwareEngineerAndrew-School <39067@minutemanstudent.org>
2026-03-29 23:10:23 -0400

    Add files via upload

---
+++ mapRenderingDicts.py
@@ -0,0 +1,37 @@
+roomInitialsDict={
+  "normal":"N",
+  "puzzle":"P",
+  "fairy":"F",
+  "entrance":"E",
+  "yellow":"M",
+  "blood":"B",
+  "trap":"T",
+  "rare":"R"
+
+
+  }
+roomColorDict={
+  "N":"DEFINITION",
+  "P":"BUILTIN",
+  "F":"hit",
+  "E":"STRING",
+  "M":"KEYWORD",
+  "B":"COMMENT",
+  "T":"stderr",
+  "R":"console",
+
+  }
+vertDoorInitialDict={
+  "normal":"|",
+  "wither":"⋮",
+  "blood":"⦀",
+  "entrance":"⯼",
+  "none":" "
+  }
+horizontalDoorInitialDict={
+  "normal":"-",
+  "wither":"⋯",
+  "blood":"≡",
+  "entrance":"⯺",
+  "none":" "
+  }
